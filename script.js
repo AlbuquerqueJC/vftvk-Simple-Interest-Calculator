0d3b44f0eaa5915b5a4dc690c7d6d6b969fa16bc
John A <john.albuquerque@gmail.com>
2022-02-09 12:35:58 -0500

    Added validation for principal and number of years.
    Fixed CSS and layout issues.

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     // Setup values
     //compute();
+
+    // Range values
+    const slider = document.getElementById("rate");
+    const output = document.getElementById("rate_val");
+    output.innerHTML = slider.value + '%';
+
+    slider.oninput = function() {
+        output.innerHTML = this.value + '%';
+    }
 });
 
 // Simple Interest Calculator A = P(1 + rt)
@@ -11,24 +20,45 @@ function compute()
 {
     // Get current year
     const d = new Date();
-    let currentYear = d.getFullYear().toPrecision();
+    let currentYear = d.getFullYear();
 
-    // Assign form values to related variables
+    // Check and Assign form values to related variables
     let principal = document.getElementById("principal").value;
     let rate = document.getElementById("rate").value;
     let years = document.getElementById("years").value;
+
+    const pError = document.getElementById('error');
+    const yError = document.getElementById('yearError');
+
+    // validation fails if the principal input is 0 or less
+    // Principal amount validation
+    if (principal < 1) {
+        pError.innerHTML = "Please enter a positive number";
+        pError.style.display = 'block';
+        return false;
+    } else {
+        pError.style.display = 'none';
+    }
+    // Number of years validation
+    if (parseFloat(years) < 1 || isNaN(years) ) {
+        yError.innerHTML = "Please enter a positive number";
+        yError.style.display = 'block';
+        return false;
+    } else {
+        yError.style.display = 'none';
+    }
 
     // Calculate Interest and Total
     const total = principal * (1 + ((rate/100) * years));
     const interest = total - principal;
 
     // Calculate Projected Year
-    let projectedYear = Number(currentYear) + Number(years);
+    let projectedYear = Number(currentYear) + parseInt(years);
 
     // Assign results
     const text = `If you deposit <mark>${principal}</mark>,<br />
             at an interest rate of <mark>${rate}%</mark>.<br />
-            You will receive an amount of <mark>${interest}</mark>,<br />
+            You will receive an amount of <mark>${interest.toFixed(2)}</mark>,<br />
             in the year <mark>${projectedYear}</mark>`
 
     // Print results
